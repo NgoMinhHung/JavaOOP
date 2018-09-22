4570eb16b5d341fd90702ae836b69feeca39caf9
Minh Hung <ngominhhung97@gmail.com>
2018-09-22 15:02:25 +0700

    add ex1

---
+++ Ex1/src/ex1/Triangle.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex1;
+
+/**
+ *
+ * @author Hung
+ */
+public class Triangle {
+    private int a;
+    
+    private int b;
+    
+    private int c;
+    
+    public int getA()
+    {
+        return a;
+    }
+    
+    public void setA(int a)
+    {
+        this.a= a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+}
